d20a0859cc3c9649ca23f1fcb5fbbd1dd2032f6d
Norbert Reschke <reschke@hsbkob.de>
2020-09-16 15:28:46 +0200

    neue Datei:     CONCEPT-theme.yml neue Datei:     concept.sh neue Datei:     fonts/LICENSE-mplus-testflight-58 neue Datei:     fonts/LICENSE-noto-2015-06-05 neue Datei:     fonts/Muli-Black.ttf neue Datei:     fonts/Muli-BlackItalic.ttf neue Datei:     fonts/Muli-Bold.ttf neue Datei:     fonts/Muli-BoldItalic.ttf neue Datei:     fonts/Muli-ExtraBold.ttf neue Datei:     fonts/Muli-ExtraBoldItalic.ttf neue Datei:     fonts/Muli-ExtraLight.ttf neue Datei:     fonts/Muli-ExtraLightItalic.ttf neue Datei:     fonts/Muli-Italic.ttf neue Datei:     fonts/Muli-Light.ttf neue Datei:     fonts/Muli-LightItalic.ttf neue Datei:     fonts/Muli-Regular.ttf neue Datei:     fonts/Muli-SemiBold.ttf neue Datei:     fonts/Muli-SemiBoldItalic.ttf neue Datei:     fonts/OFL.txt neue Datei:     fonts/mplus1mn-bold-ascii.ttf neue Datei:     fonts/mplus1mn-bold_italic-ascii.ttf neue Datei:     fonts/mplus1mn-italic-ascii.ttf neue Datei:     fonts/mplus1mn-regular-ascii-conums.ttf neue Datei:     fonts/mplus1p-regular-fallback.ttf neue Datei:     fonts/notoserif-bold-subset.ttf neue Datei:     fonts/notoserif-bold_italic-subset.ttf neue Datei:     fonts/notoserif-italic-subset.ttf neue Datei:     fonts/notoserif-regular-subset.ttf neue Datei:     images/Leistungskonzept.png neue Datei:     images/dead-link.png neue Datei:     images/drcm.png neue Datei:     images/fossil.png neue Datei:     images/git.png

---
+++ concept.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# script to get structure and history set correct for protocoling conferences 
+
+file=$1
+yamlfile="CONCEPT-theme.yml"
+
+if [ ! -f $yamlfile ]
+then
+   echo "$yamlfile must exist"
+   exit
+fi 
+
+if [ -z "$file" ]
+then
+   echo "Syntax:   Scriptname filename"
+   echo "Beispiel: $0 somefile.asciidoc"
+   exit
+fi
+
+if [ ! -f $file ]
+then
+   echo "$file must exist"
+   exit
+fi
+
+echo $file
+
+git add $file
+git commit $file
+commit="$(git log -n 1 --date=iso8601 --grep $file|grep commit|cut -d" " -f 2|cut -c-7)"
+author="$(git log -n 1 --date=iso8601 --grep $file|grep -i author|cut -d" " -f 4)"
+date="$(git log -n 1 --date=iso8601 --grep $file|grep -i date|cut -d" " -f 4)"
+time="$(git log -n 1 --date=iso8601 --grep $file|grep -i date|cut -d" " -f 5)"
+echo ""
+echo "last changing commit of $file is: $commit"
+echo "last changing editor of $file is: $author"
+echo "last changing date   of $file is: $date"
+echo "last changing time   of $file is: $time"
+echo ""
+#echo "sed is working"
+sed -i \
+  -e 's/^\(\s*commit\s*:\s*\).*/\1'\"$commit'\"/' \
+  -e 's/^\(\s*author\s*:\s*\).*/\1'\"$author'\"/' \
+  -e 's/^\(\s*date\s*:\s*\).*/\1'\"$date'\"/' \
+  -e 's/^\(\s*time\s*:\s*\).*/\1'\"$time'\"/' \
+  $yamlfile
+#echo "sed is ready"
+
+filebase=$(basename $file .asciidoc)
+hmtl=${filebase}.hmtl
+pdf=${filebase}.pdf
+echo "asciidoctor starts working on $file"
+asciidoctor -a imagesdir=images $file
+echo ""
+echo "asciidoctor has made $hmtl"
+echo ""
+
+echo "asciidoctor-pdf starts working on $file"
+asciidoctor-pdf -a pdf-style=CONCEPT -a pdf-stylesdir=. -a pdf-fontsdir=fonts -a imagesdir=images $file
+echo ""
+echo "asciidoctor has made $pdf"
+echo ""
+big=$(ls -al --block-size=1K $pdf|cut -d" " -f5)
+echo "hexapdf starts optimizing $pdf"
+hexapdf optimize $pdf hexa.pdf && mv hexa.pdf $pdf
+echo "hexapdf has work done and"
+small=$(ls -al --block-size=1K $pdf|cut -d" " -f5)
+echo "now size (in K) of $pdf shrinked from $big to $small"
+
+#restore yamlfile
+#echo "sed is working"
+sed -i \
+  -e 's/^\(\s*commit\s*:\s*\).*/\1'\"dummy'\"/' \
+  -e 's/^\(\s*author\s*:\s*\).*/\1'\"dummy'\"/' \
+  -e 's/^\(\s*date\s*:\s*\).*/\1'\"dummy'\"/' \
+  -e 's/^\(\s*time\s*:\s*\).*/\1'\"dummy'\"/' \
+  $yamlfile
+#echo "sed is ready"
+
